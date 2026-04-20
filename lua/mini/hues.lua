dc0b8f54c2cd3580625418f5f1ecda55dcab2c6e
jy <nicholasxjy@gmail.com>
2026-04-20 11:22:47 +0800

    Add SnacksPickerListTitle and SnacksPickerInputPrompt highlights

--- lua/mini/hues.lua
+++ lua/mini/hues.lua
@@ -1130,12 +1130,14 @@ MiniHues.apply_palette = function(palette, plugins, opts)
     hi("SnacksPickerPreviewTitle", { fg = p.blue, bg = p.bg, bold = true })
     hi("SnacksPickerInputTitle", { fg = p.red, bg = p.bg, bold = true })
     hi("SnacksPickerBoxTitle", { fg = p.red, bg = p.bg, bold = true })
+	hi("SnacksPickerListTitle", { fg = p.green, bg = p.bg, bold = true })
 
     hi("SnacksPickerPreviewBorder", { fg = p.bg_edge, bg = p.bg_edge })
     hi("SnacksPickerListBorder", { fg = p.bg_edge, bg = p.bg_edge })
     hi("SnacksPickerBoxBorder", { fg = p.bg_edge, bg = p.bg_edge })
     hi("SnacksPickerInputBorder", { fg = p.bg_edge, bg = p.bg_edge })
     hi("SnacksPickerInput", { fg=p.accent, bg=p.bg_edge, bold=true })
+	hi("SnacksPickerInputPrompt", { fg=p.accent, bg=p.bg, bold=true })
 
   end
 

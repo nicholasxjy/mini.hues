797c130568a30bb24baf65c1ed97f4bda77db027
nick <nicholasxjy@gmail.com>
2026-05-20 20:43:30 +0800

    Expand palette generation to 12 hues

--- colors/randomhue.lua
+++ colors/randomhue.lua
@@ -8,7 +8,7 @@ local base_colors = hues.gen_random_base_colors()
 hues.setup({
   background = base_colors.background,
   foreground = base_colors.foreground,
-  n_hues = 8,
+  n_hues = 12,
   saturation = vim.o.background == 'dark' and 'medium' or 'high',
   accent = 'bg',
 })
